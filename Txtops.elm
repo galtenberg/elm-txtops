8a4f72a34d5828280b732f9711c2b9a0e542d72c
Christopher Galtenberg <galtenbergs@gmail.com>
2016-12-03 20:06:10 -0800

    Combined view

--- Txtops.elm
+++ Txtops.elm
@@ -1,9 +1,8 @@
-module App exposing (..)
+--module App exposing (..)
 
 import Html exposing (..)
-import Html.App
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput, onBlur)
+import Html.Events exposing (..)
 import String exposing (..)
 
 -------------------------------------------------------
@@ -19,35 +18,14 @@ splitStrings str = split "\n\n" str
 liText l = li [] [text l]
 
 renderList : ListModel -> Html MsgTxt
---renderList : ListModel -> Html MsgNoOp
---renderList : ListModel -> Html a
 renderList list =
     list
     |> List.map liText
     |> ul []
 
--- MESSAGES
---type MsgNoOp = NoOp
-
--- UPDATE
---listUpdate : MsgNoOp -> ListModel -> ( ListModel, Cmd MsgNoOp )
---listUpdate msg model =
-    --case msg of
-        --NoOp ->
-            --( model, Cmd.none )
-
 -- VIEW
 listView : ListModel -> Html MsgTxt
---listView : ListModel -> Html MsgNoOp
 listView listModel = renderList listModel
-
--- SUBSCRIPTIONS
---listSubscriptions : ListModel -> Sub MsgNoOp
---listSubscriptions model = Sub.none
-
--- INIT
---listInit : ( ListModel, Cmd MsgNoOp )
---listInit = ( splitStrings "Hello\n\nxorld", Cmd.none )
 
 
 -------------------------------------------------------
@@ -64,17 +42,12 @@ txtUpdate : MsgTxt -> TxtOpsModel -> ( TxtOpsModel, Cmd MsgTxt )
 txtUpdate (NewTxt txt) oldTxt =
   ( txt, Cmd.none )
 
---blurHappens : Sub MsgTxt
---blurHappens = ("hi")
-
 -- VIEW
 txtOpsView : TxtOpsModel -> Html MsgTxt
---txtOpsView : TxtOpsModel -> Html a
 txtOpsView txtOpsModel = div []
     [
       input [ cols 40, placeholder "Magic box" ] [ ]
-    --, button [ ] [ "sup" ]
-    --, renderList (splitStrings "Hello\n\nxorld")
+    , button [ onClick(NewTxt "hi") ] [ text "hi" ]
     , listView (splitStrings "Hello\n\nxorld")
     , textarea [cols 40, rows 10, placeholder "Feed me txtops", onInput NewTxt ] [ text txtOpsModel ]
     ]
@@ -91,33 +64,12 @@ txtInit = ( "abc", Cmd.none )
 -------------------------------------------------------
 -- Page
 
---combinedView : (Html a, Html a) -> (Html a)
---combinedView (diva, divb) =
-    --div [] [ [ diva, divb ] ]
-
--- INIT
---combinedInit : ( 
-
--- MAIN
-
---main1 : Program Never
---main1 =
-    --Html.App.program
-        --{
-        --init = listInit
-        --, view = listView
-        --, update = listUpdate
-        --, subscriptions = listSubscriptions
-        --}
-
-main : Program Never
+main : Program Never TxtOpsModel MsgTxt
 main =
-    Html.App.program
-    --Html.App.beginnerProgram
+    Html.program
         {
           init = txtInit
         , view = txtOpsView
-        --, view = combinedView(listView, txtOpsView)
         , update = txtUpdate
         , subscriptions = txtSubscriptions
         }
